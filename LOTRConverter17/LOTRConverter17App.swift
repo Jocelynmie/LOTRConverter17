30bba6ec38a1feb2ab89553a998529f76647ff85
jocleyn123 <jocelynyang414@outlook.com>
2024-10-15 17:01:26 -0700

    Initial Commit

---
+++ LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by JocleynYang on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
